fa3a11e2ef9b3a4a63aed833904da7cc37c8243d
Vishnu Ramankutty <vishnuramankutty@Vishnus-MBP.bbrouter>
2022-12-14 18:56:06 +0530

    setup

---
+++ web-viewer/src/config.js
@@ -0,0 +1,42 @@
+window.addEventListener("viewerLoaded", function () {
+  console.log("Viewer Loaded");
+  console.log("instance", instance);
+  return instance;
+});
+
+window.addEventListener("documentLoaded", () => {
+  const annotManager = docViewer.getAnnotationManager();
+  const rectangle = new Annotations.RectangleAnnotation();
+  rectangle.PageNumber = 2;
+  rectangle.X = 100;
+  rectangle.Y = 100;
+  rectangle.Width = 250;
+  rectangle.Height = 250;
+  rectangle.Author = annotManager.getCurrentUser();
+  annotManager.addAnnotation(rectangle);
+});
+
+function receiveMessage(event) {
+  if (
+    event.isTrusted &&
+    typeof event.data === "object" &&
+    event.origin === "http://localhost:3000"
+  ) {
+    switch (event.data.type) {
+      case "OPEN_DOCUMENT_URL":
+        const { url } = event.data;
+        event.target.readerControl.loadDocument(url);
+        break;
+      case "CLOSE_DOCUMENT":
+        event.target.readerControl.closeDocument();
+        break;
+      case "GET_INSTANCE":
+        console.log("qqq");
+        return instance;
+      default:
+        break;
+    }
+  }
+}
+
+window.addEventListener("message", receiveMessage, false);
--- web-viewer/src/viewer.js
+++ web-viewer/src/viewer.js
@@ -1,32 +1,52 @@
 import React, { useEffect, useRef } from "react";
 import WebViewer from "@pdftron/webviewer";
-
+import { Button, Col, Container, Row } from 'reactstrap';
 const Viewer = () => {
   const viewer = useRef(null);
+  let instance;
   useEffect(() => {
-    console.log("1");
     handleWebViewer();
   }, []);
 
-  const handleWebViewer = () => {
-    let params={
-        path: "http://localhost:8080/lib/",
-        // path: "webviewer/lib/",
-        //   config: "http://localhost:8080/config.js",
-    }
-      WebViewer(params,viewer.current)
-        .then(async(response) => {
-          console.log(response);
-        })
-        .catch((err) => {
-          console.log("Error", err);
-        });
-    } 
+  const handleWebViewer = async () => {
+    let params = {
+      path: "/webviewer/lib",
+      config: "./config.js",
+      initialDoc: "https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf",
+      disabledElements: [
+        'ribbons',
+        'toggleNotesButton',
+        'searchButton',
+        'menuButton',
+        'rubberStampToolGroupButton',
+        'stampToolGroupButton',
+        'fileAttachmentToolGroupButton',
+        'calloutToolGroupButton',
+        'undo',
+        'redo',
+        'eraserToolButton', 'header'
+      ],
+
+    };
+    WebViewer(params, viewer.current).then((response) => {
+      console.log('reponse', response)
+      instance = response;
+    });
+
+  };
+
 
   return (
-    <div>
-      <div className="webviewer" ref={viewer}></div>
-    </div>
+    <Container>
+      <Row>
+        <Col xs='12'>
+          <Button color="primary" size="lg">Large Button</Button>
+        </Col>
+        <Col xs='12'>
+          <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
